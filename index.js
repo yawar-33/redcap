e393eb912f4de8ce2d26dbd898f408ec553758b2
Yawar Ali <55242837+yawar-33@users.noreply.github.com>
2023-07-08 01:16:58 +0500

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use("/api", defaultroutes)
 app.use("/api", adminRoutes)
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running at ${PORT}`);
 })
