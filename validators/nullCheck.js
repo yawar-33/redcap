da07efa94eda9595a464222139e76069786ccd5a
Yawar Ali <55242837+yawar-33@users.noreply.github.com>
2023-07-08 01:05:13 +0500

    Update nullCheck.js

--- validators/nullCheck.js
+++ validators/nullCheck.js
@@ -1,15 +1,23 @@
-module.exports = {
-    isNull(value) {
-        if (
-            value === '' ||
-            value === 0 ||
-            value === 'Null' ||
-            value === null ||
-            value === undefined
-        ) {
-            return true
-        } else {
-            return false
-        }
+
+const isNull = (value) => {
+    if (
+        value === '' ||
+        value === 0 ||
+        value === 'Null' ||
+        value === null ||
+        value === undefined ||
+        value === 'Please Select' ||
+        value.length === 0 ||
+        value === -1 ||
+        value === 'Please Coverage' ||
+        value === 'Please Relationship'
+    ) {
+        return true
+    } else {
+        return false
     }
-};
+}
+
+
+
+module.exports = { isNull }
