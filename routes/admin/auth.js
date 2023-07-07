cb3881d353e4f8d7bfd441a86a907512944c096d
Yawar Ali <55242837+yawar-33@users.noreply.github.com>
2023-07-08 01:25:46 +0500

    Update auth.js

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -2,16 +2,16 @@ const express = require('express');
 // const { signup, signin, signout, getToken, addToken } = require('../../controller/admin/admin');
 const { signin } = require('../../controller/admin/admin');
 // const { isSignupRequestValidated, isSignInRequestValidated } = require('../../validators/userValidations');
-// const { requireSignin } = require('../../validators/validateToken');
+const { requireSignin } = require('../../validators/validateToken');
 const router = express.Router();
 
 
-// router.post('/admin/signup',  signup);
+router.post('/admin/signup',  signup);
 
 router.post('/admin/signin', signin);
 
-// router.get('/admin/gettoken', requireSignin, getToken)
-// router.post('/admin/addToken', requireSignin, addToken)
+router.get('/admin/gettoken', requireSignin, getToken)
+router.post('/admin/addToken', requireSignin, addToken)
 
 // router.post('/admin/signout', signout);
 
